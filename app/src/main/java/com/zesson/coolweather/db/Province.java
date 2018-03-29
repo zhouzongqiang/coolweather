e01e3a97cd2d74e76c73feff38ebddf075e031fd
Zesson <Zesson@gmail.com>
2018-03-29 22:44:40 +0800

    First commit.

---
+++ app/src/main/java/com/zesson/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.zesson.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/3/15.
+ */
+
+public class Province extends DataSupport
+{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
